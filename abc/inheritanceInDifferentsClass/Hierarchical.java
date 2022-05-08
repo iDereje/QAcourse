5bb387e626e3eb5a0d4d5752f4ed195fb31f3564
assafbd <assafbd@ASSAF-LP-MG.MatrixGlobal.com>
2022-05-09 02:41:12 +0300

    e

--- abc/inheritanceInDifferentsClass/Hierarchical.java
+++ abc/inheritanceInDifferentsClass/Hierarchical.java
@@ -3,7 +3,6 @@ package inheritanceInDifferentsClass;
 class Hierarchical
 {
 
-	
     public static void main(String args[])
     {
         System.out.println("******************** Displaying Person Information ********************");
